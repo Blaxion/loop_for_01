512d5a4ce9c31953ff6fea560304857f1d6433b7
Blaxion <khibaziev.f@gmail.com>
2021-04-21 23:46:19 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Exo 1
+// let tab = ["Farhad","Yassin","Alexis","Kevin","Junior","Andy","Tania","Issam","Mohammed", "Mihai", "Loic", "Charles", "William","Oussama","Ilyas","Anthony","Dorian","Alain","Cactus"]
+
+// for (let i = 0; i < tab.length; i++) {
+//     const element = tab[i];
+//     console.log("Bonjour", element)
+// }
+
+// //  Exo 2
+// let int = prompt("Un chiffre maggle")
+// for (let i = 0; i <= int; i++) {
+//     console.log (i)
+// }
+
